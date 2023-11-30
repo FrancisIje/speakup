2859e5f984883d8092c7c1151f750a1cf62f2380
FrancisIje <ijenebefrancis@gmail.com>
2023-11-30 23:35:24 +0100

    switch btw verifyemail and language screens

---
+++ lib/screens/signup/verify_builder.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:speakup/screens/signup/language.dart';
+import 'package:speakup/screens/signup/verify.dart';
+
+class VerifyBuilder extends StatefulWidget {
+  const VerifyBuilder({super.key});
+
+  @override
+  State<VerifyBuilder> createState() => _VerifyBuilderState();
+}
+
+class _VerifyBuilderState extends State<VerifyBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 2)),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const VerifyEmailScreen();
+
+          case ConnectionState.active:
+          case ConnectionState.done:
+            return const LanguageScreen();
+        }
+      },
+    );
+  }
+}
