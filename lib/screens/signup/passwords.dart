afa9bb0734b0d4918c3d7a1b7aea6120b8f4d475
FrancisIje <ijenebefrancis@gmail.com>
2023-12-01 15:48:46 +0100

    password screen

---
+++ lib/screens/signup/passwords.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:speakup/screens/signup/tutor.dart';
+
+class PasswordSetScreen extends StatefulWidget {
+  const PasswordSetScreen({super.key});
+
+  @override
+  State<PasswordSetScreen> createState() => _PasswordSetScreenState();
+}
+
+class _PasswordSetScreenState extends State<PasswordSetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "SET YOUR PASSWORD",
+                  style:
+                      TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Text(
+                  "Kindly enter and confirm your password to \ncontinue",
+                  style:
+                      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value != null) {
+                      return 'Enter password';
+                    } else {
+                      return null;
+                    }
+                  },
+
+                  // controller: emailTextController,
+                  decoration: InputDecoration(
+                      hintStyle: const TextStyle(color: Color(0xFF777777)),
+                      hintText: "Enter password",
+                      fillColor: Colors.white,
+                      filled: true,
+                      suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(16.r)))),
+                ),
+                SizedBox(
+                  height: 14.h,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value != null) {
+                      return 'Confirm password';
+                    } else {
+                      return null;
+                    }
+                  },
+                  // controller: emailTextController,
+                  decoration: InputDecoration(
+                      hintStyle: const TextStyle(color: Color(0xFF777777)),
+                      hintText: "Confirm password",
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(16.r)))),
+                ),
+                const Expanded(child: SizedBox()),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const TutorScreen(),
+                    ));
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(
+                        Color.fromRGBO(104, 73, 255, 1)),
+                    fixedSize:
+                        MaterialStatePropertyAll(Size(double.maxFinite, 50.h)),
+                    shape: const MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(40),
+                                right: Radius.circular(40)))),
+                  ),
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.sp),
+                  ),
+                ),
+                SizedBox(
+                  height: 60.h,
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
