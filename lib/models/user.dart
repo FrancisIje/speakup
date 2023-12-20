9df3f7d8656b45379a48b4015001d9e273b0dc3c
FrancisIje <ijenebefrancis@gmail.com>
2023-12-20 01:41:55 +0100

    user model

---
+++ lib/models/user.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SpeakupUser {
+  String firstName;
+  String lastName;
+  String email;
+  String phoneNumber;
+  String targetLanguage;
+  String nativeLanguage;
+  String tutorGender;
+  String profilePictureUrl; // Firebase Storage URL
+
+  SpeakupUser(
+      {required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.phoneNumber,
+      required this.targetLanguage,
+      required this.nativeLanguage,
+      required this.profilePictureUrl,
+      required this.tutorGender});
+
+  Map<String, dynamic> toSpeakupUserMap() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'targetLanguage': targetLanguage,
+      'nativeLanguage': nativeLanguage,
+      'profilePictureUrl': profilePictureUrl,
+      'tutorGender': tutorGender,
+    };
+  }
+
+  static SpeakupUser fromSpeakupUserMap(Map<String, dynamic> map) {
+    return SpeakupUser(
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      email: map['email'],
+      phoneNumber: map['phoneNumber'],
+      targetLanguage: map['targetLanguage'],
+      nativeLanguage: map['nativeLanguage'],
+      profilePictureUrl: map['profilePictureUrl'],
+      tutorGender: map['tutorGender'],
+    );
+  }
+}
