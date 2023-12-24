8519cb965040f6bc472717989d53090a1cc664b9
FrancisIje <ijenebefrancis@gmail.com>
2023-12-24 14:48:44 +0100

    video state

---
+++ lib/provider/video_state.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoState extends ChangeNotifier {
+  VideoPlayerController? _speakingController;
+  VideoPlayerController? _listeningController;
+
+  bool get isInitialized =>
+      _speakingController != null && _listeningController != null;
+
+  VideoPlayerController? get controller =>
+      isTalking ? _speakingController : _listeningController;
+
+  bool isTalking = false;
+
+  void initializeControllers() {
+    _speakingController =
+        VideoPlayerController.asset("videos/speaking_woman.mp4");
+    _listeningController =
+        VideoPlayerController.asset("videos/listening_woman.mp4");
+
+    _speakingController!.initialize().then((_) {
+      notifyListeners();
+    });
+    _listeningController!.initialize().then((_) {
+      notifyListeners();
+    });
+  }
+
+  void setTalking(bool value) {
+    isTalking = value;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _speakingController?.dispose();
+    _listeningController?.dispose();
+    super.dispose();
+  }
+}
