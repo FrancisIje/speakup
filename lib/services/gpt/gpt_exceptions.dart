d63e50e1663c74c6418b57a1fb15edc718bdd33d
FrancisIje <ijenebefrancis@gmail.com>
2024-01-08 22:45:08 +0100

    exception file for chatgpt class

---
+++ lib/services/gpt/gpt_exceptions.dart
@@ -0,0 +1,11 @@
+class CouldNotGetChatResponse implements Exception {}
+
+class CouldNotGetWordTranslation implements Exception {}
+
+class CouldNotGetRoleplayResponse implements Exception {}
+
+class CouldNotGetParagraphResponse implements Exception {}
+
+class CouldNotFetchSpeech implements Exception {}
+
+class CouldNotTranscribeAudio implements Exception {}
